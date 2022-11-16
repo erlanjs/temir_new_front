8d66c60daa78d4a4ef55208179d5d6a6f4134308
ymankulov <ymankulov253546@gmail.com>
2022-11-16 11:14:33 +0600

    footer

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import HomeHero from "./homeHero";
 import ContactUs from "./contactUs";
+import Footer from "./footer";
 
 const Home = () => {
     return (
         <>
             <HomeHero/>
             <ContactUs/>
+            <Footer/>
         </>
     );
 };
