0598122b72070edd367df3c3d2d748479052a376
unknown <ymankulov>
2022-11-30 08:48:07 +0600

    Media Section Contact

--- src/pages/home/Contact.tsx
+++ src/pages/home/Contact.tsx
@@ -22,15 +22,15 @@ const Contact = () => {
                     <h1 className="font-bold text-4xl tracking-wide text-white py-14 text-center">Contact us</h1>
                     {
                         name.map(el => (
-                            <div>
+                            <div className="w-[80%] md:w-[40%]">
                                 <h5 className="font-light text-sm pb-1.5 tracking-wide">{el.title}</h5>
                                 <input type="text" placeholder={el.title}
-                                       className="focus:shadow-lg focus:shadow-gray-700 w-96 py-1.5 bg-transparent border-b-2 placeholder-gray-500 mb-7 outline-0 pl-1.5"/>
+                                       className="focus:shadow-lg focus:shadow-gray-700 w-[100%] py-1.5 bg-transparent border-b-2 placeholder-gray-500 mb-7 outline-0 pl-1.5"/>
                             </div>
                         ))
                     }
 
-                    <button className="w-56 h-14 bg-black font-normal text-lg text-white my-10 rounded-md hover:scale-110 hover:bg-transparent transition duration-500 ease-in-out" style={{
+                    <button className="md:w-56 md:h-14 w-40 h-10  bg-black font-normal text-lg text-white my-10 rounded-md hover:scale-110 hover:bg-transparent transition duration-500 ease-in-out" style={{
                         boxShadow: "-12.8629px -10.5242px 24.5564px rgba(72, 72, 72, 0.25), 7.01613px 8.18548px 24.5564px #000000"
                     }}>Submit
                     </button>
