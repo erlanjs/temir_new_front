c4983babcd7d93441f0a29a513f65c3dd15c660d
kabyl-ulan <kabylulanabdykalykuulu@gmail.com>
2022-12-10 21:21:36 +0600

    change aboutPage, add Loader

--- src/pages/basket/BasketTotal.tsx
+++ src/pages/basket/BasketTotal.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { FC } from 'react'
 
-const BasketTotal = () => {
+interface IProps {
+  totalPrice: number
+}
+
+const BasketTotal: FC<IProps> = ({ totalPrice }) => {
   return (
     <div className="w-full flex justify-end py-2">
       <div className="w-[50%] max-md:w-full flex justify-between items-center px-8">
         <p className="font-[900] text-[30px]">Total:</p>
-        <p className="font-[900] text-[30px] uppercase">400 AED</p>
+        <p className="font-[900] text-[30px] uppercase">{totalPrice} AED</p>
       </div>
     </div>
   )
--- src/pages/checkout/components/YourOrders.tsx
+++ src/pages/checkout/components/YourOrders.tsx
@@ -1,35 +1,15 @@
 import React, { FC } from 'react'
+import { useAppSelector } from '../../../hooks'
 interface IProps {
   register: any
 }
 
 const YourOrders: FC<IProps> = ({ register }) => {
-  const basket = [
-    {
-      id: 1,
-      image: 'TemirBlack',
-      name: 'Premium Smart card',
-      view: 'Gold Brushed',
-      price: 200,
-      quantity: 1,
-    },
-    {
-      id: 2,
-      image: 'TemirBlack',
-      name: 'Premium Smart card',
-      view: 'Gold Brushed',
-      price: 200,
-      quantity: 1,
-    },
-    {
-      id: 3,
-      image: 'TemirBlack',
-      name: 'Premium Smart card',
-      view: 'Gold Brushed',
-      price: 200,
-      quantity: 1,
-    },
-  ]
+  const { basket } = useAppSelector((state) => state.ReducerBasket)
+  let totalPrice = basket.reduce(
+    (a: any, b: any) => +a.price * a.quantity + +b.price * b.quantity
+  )
+
   return (
     <div className="bg-transparent pt-6">
       <h1 className="font-[Arial] font-[900] text-[30px] max-md:text-[24px] text-center">
@@ -42,7 +22,7 @@ const YourOrders: FC<IProps> = ({ register }) => {
       <div className="relative py-5">
         <div className="before:content-[''] absolute border border-white rounded-sm top-0 left-0 right-0"></div>
         <div className="max-md:rounded-[10px] max-md:bg-[#161616] max-md:mx-4">
-          {basket.map((el) => (
+          {basket.map((el: any) => (
             <div className="w-[90%] mx-auto" key={el.id}>
               <div className="flex justify-between items-center ">
                 <div className="bg-[#161616] w-[60%] max-md:w-[70%] py-2 pl-4 max-md:pl-0">
@@ -58,7 +38,9 @@ const YourOrders: FC<IProps> = ({ register }) => {
                 <div className="w-[30%]">
                   <div className="border-b-[1px] w-[96%] border-solid border-[#1E1E1E] flex justify-between max-md:justify-center max-md:border-none text-[30px] max-lg:text-[24px] max-md:text-[20px] max-md:whitespace-nowrap">
                     <p className="uppercase text-[#363638]">{el.quantity}x</p>
-                    <p className="uppercase max-md:text-[#363638]">200 AED</p>
+                    <p className="uppercase max-md:text-[#363638]">
+                      {el.price} AED
+                    </p>
                   </div>
                 </div>
               </div>
@@ -67,7 +49,7 @@ const YourOrders: FC<IProps> = ({ register }) => {
         </div>
         <div className="font-[Arial] text-[30px] max-md:text-[24px] flex justify-between w-[90%] mx-auto px-4 py-5">
           <h3>Total:</h3>
-          <h3 className="uppercaes">600 AED</h3>
+          <h3 className="uppercaes">{totalPrice} AED</h3>
         </div>
       </div>
     </div>
