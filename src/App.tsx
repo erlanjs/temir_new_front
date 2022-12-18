2b49deb34c99406f84fd12ed260b22d4ad4bdc0c
kabyl-ulan <kabylulanabdykalykuulu@gmail.com>
2022-12-18 13:38:49 +0600

    change components

--- src/App.tsx
+++ src/App.tsx
@@ -58,7 +58,7 @@ function App() {
   ];
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<ProductPage />} />
